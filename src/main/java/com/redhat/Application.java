251096280aa50df4f7e353b30561d3ade2225677
Andres Romero <aromero@redhat.com>
2019-09-11 15:24:36 -0400

    Using toCharArray()

--- src/main/java/com/redhat/Application.java
+++ src/main/java/com/redhat/Application.java
@@ -31,10 +31,9 @@ public class Application {
 
     // must have a main method spring-boot can run
     public static void main(String[] args) throws GeneralSecurityException, IOException {
-    	char[] password = {'p', 'a', 's', 's', 'w', 'o', 'r', 'd'};
     	TrustStore.createFromCrtFile("/tmp/src/src/main/resources/ca.crt",
     								 "/tmp/src/src/main/resources/keystore.jks",
-    								 password);
+    								 "password".toCharArray());
         SpringApplication.run(Application.class, args);
     }
 
