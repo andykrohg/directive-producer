6255e9853c7f15516dbb2a4f0e9574ec504f957e
Andres Romero <aromero@redhat.com>
2019-09-11 15:20:44 -0400

    Adding auto--generate keystore logic - Part Deux

--- src/main/java/com/redhat/TrustStore.java
+++ src/main/java/com/redhat/TrustStore.java
@@ -1,11 +1,9 @@
 package com.redhat;
 
-import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
