69b98ab7bf71fcbf2802e43e4057ddb7b6bce89a
Andy Krohg <akrohg@redhat.com>
2019-09-23 15:17:10 -0400

    Add killswitch implementation to finish a game

--- src/main/java/com/redhat/ProducerRoute.java
+++ src/main/java/com/redhat/ProducerRoute.java
@@ -17,28 +17,31 @@ public class ProducerRoute extends RouteBuilder {
 	@Override
 	public void configure() throws Exception {
 		restConfiguration().component("servlet").bindingMode(RestBindingMode.json);
-		
-		KafkaComponent kafka = new KafkaComponent();		
+
+		KafkaComponent kafka = new KafkaComponent();
 		KafkaConfiguration kafkaConfig = new KafkaConfiguration();
 		kafkaConfig.setBrokers("my-cluster-kafka-bootstrap-myproject.apps.akrohg-openshift.redhatgov.io:443");
 		kafkaConfig.setSecurityProtocol("SSL");
 		kafkaConfig.setSslTruststoreLocation("/tmp/src/src/main/resources/keystore.jks");
 		kafkaConfig.setSslTruststorePassword("password");
 		kafka.setConfiguration(kafkaConfig);
-		
+
 		getContext().addComponent("kafka", kafka);
-		
+
 		rest("/rest")
-		.post("/produce/{color}").route().streamCaching().process(new Processor() {
-			@Override
-			public void process(Exchange exchange) throws Exception {
-				Message message = exchange.getIn();
-				log.info("Received message: {}", message.getBody(String.class));
-				
-				message.setBody(new ObjectMapper().writeValueAsString(exchange.getIn().getBody()));
-				message.setHeader(KafkaConstants.PARTITION_KEY, 0);
-				message.setHeader(KafkaConstants.KEY, "Camel");
-			}
-		}).recipientList(simple("kafka:directive-${header.color}")).setBody(constant("Message sent successfully."));
-	}
+			.post("/produce/{color}").route().streamCaching().process(new Processor() {
+				@Override
+				public void process(Exchange exchange) throws Exception {
+					Message message = exchange.getIn();
+					log.info("Received message: {}", message.getBody(String.class));
+	
+					message.setBody(new ObjectMapper().writeValueAsString(exchange.getIn().getBody()));
+					message.setHeader(KafkaConstants.PARTITION_KEY, 0);
+					message.setHeader(KafkaConstants.KEY, "Camel");
+				}
+			}).recipientList(simple("kafka:directive-${header.color}")).setBody(constant("Message sent successfully.")).endRest()
+			
+			//killswitch /camel/rest/gameOver/{color}
+			.get("/gameOver/{color}").route().setBody(simple("${header.color}")).to("kafka:game-over");
+		}
 }
